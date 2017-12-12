9a2d24016ad978f015a82c76ec3d2f37b771ff6c
melono6 <jason_caine08@yahoo.co.uk>
2017-12-12 16:31:01 +0000

    update xhr

--- src/client/js/actions/xhr.js
+++ src/client/js/actions/xhr.js
@@ -1,14 +1,25 @@
 /*jslint node:true, browser:true, esnext:true */
 'use strict';
 
-module.exports = (path, method, callback) => {
+module.exports = (path, method, callback, type, data) => {
     let xhr = new XMLHttpRequest();
 
     xhr.open(method, path);
     xhr.onreadystatechange = () => {
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            callback(JSON.parse(xhr.responseText));
+            if (type === 'RAW') {
+                callback(xhr.responseText);
+            } else {
+                callback(JSON.parse(xhr.responseText));
+            }
         }
     };
-    xhr.send();
+
+    if (data) {
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    } else {
+        xhr.send();
+    }
+    
 };
